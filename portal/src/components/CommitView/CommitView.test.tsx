c0cf4c1bbfef1ff284e0beb575e3b642671cdd95
Andre Sutherland <andrelsf95@gmail.com>
2019-05-10 10:05:41 +0100

    Added tests for the commit view and made the branch coverage threshold a bit more lenient as a lot of conditions are to only do one thing given a condition

---
+++ portal/src/components/CommitView/CommitView.test.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { act } from 'react-test-renderer'
+import { enzymeFind } from 'styled-components/test-utils'
+
+import CommitView from './CommitView'
+import {
+  CommitViewTextArea,
+  CommitViewButton,
+  CentredImage,
+} from './commitView.styles'
+
+describe('CommitView', (): void => {
+  it('should render without any issues', (): void => {
+    const wrapper = mount(
+      <CommitView
+        commitDescription=""
+        spec=""
+        currentSpecSHA=""
+        show
+        isCommitting={false}
+        setCurrentCommitDescription={(): void => {}}
+        commitChanges={(): void => {}}
+      />
+    )
+    expect(wrapper.find('div').length).toBeGreaterThan(0)
+  })
+
+  it('should update the current commit description when the text field is changed', (): void => {
+    const setCurrentCommitDescription = jest.fn()
+    const wrapper = mount(
+      <CommitView
+        commitDescription=""
+        spec=""
+        currentSpecSHA=""
+        show
+        isCommitting={false}
+        setCurrentCommitDescription={setCurrentCommitDescription}
+        commitChanges={(): void => {}}
+      />
+    )
+    act(
+      (): void => {
+        enzymeFind(wrapper, CommitViewTextArea).simulate('change', {
+          target: { value: 'This is a commit description' },
+        })
+      }
+    )
+    expect(setCurrentCommitDescription).toHaveBeenCalledWith(
+      'This is a commit description'
+    )
+  })
+
+  it('should commit changes if there is content in the commit description', (): void => {
+    const commitChanges = jest.fn()
+    const wrapper = mount(
+      <CommitView
+        commitDescription="This is the commit description"
+        spec="This is the specification of complete and utter madness"
+        currentSpecSHA="dasd32130sasd"
+        show
+        isCommitting={false}
+        setCurrentCommitDescription={(): void => {}}
+        commitChanges={commitChanges}
+      />
+    )
+    act(
+      (): void => {
+        enzymeFind(wrapper, CommitViewButton).simulate('click', { target: {} })
+      }
+    )
+    expect(commitChanges).toHaveBeenCalledWith(
+      'This is the commit description',
+      'This is the specification of complete and utter madness',
+      'dasd32130sasd'
+    )
+  })
+
+  it('should contain a centered image that contains a loader when we are in the process of committing changes', (): void => {
+    const wrapper = mount(
+      <CommitView
+        commitDescription="This is the commit description"
+        spec="This is the specification of complete and utter madness"
+        currentSpecSHA="dasd32130sasd"
+        show
+        isCommitting={true}
+        setCurrentCommitDescription={(): void => {}}
+        commitChanges={(): void => {}}
+      />
+    )
+    expect(enzymeFind(wrapper, CentredImage).length).toBe(1)
+  })
+})
