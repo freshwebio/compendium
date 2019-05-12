3331bb0ff754870178e4a3a6e87e6977e6c7f173
Andre Sutherland <andrelsf95@gmail.com>
2019-05-12 16:48:53 +0100

    Added tests for tooltip style rules to icon button

--- portal/src/components/IconButton/IconButton.test.tsx
+++ portal/src/components/IconButton/IconButton.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import mountWithTheme from 'testUtils/mountWithTheme'
+import { css } from 'styled-components'
 
 import IconButton from './IconButton'
 import { StyledIcon } from './iconButton.styles'
+import { StyledTooltip } from 'components/Tooltip/tooltip.styles'
 
 describe('IconButton', (): void => {
   it('should render without any issues', (): void => {
@@ -36,7 +38,37 @@ describe('IconButton', (): void => {
     expect(wrapper.find(StyledIcon)).toHaveStyleRule('opacity', '0.5')
   })
 
-  it('should present a tooltip if the button has a tooltip', (): void => {})
+  it('should present a tooltip if the button has a tooltip', (): void => {
+    const wrapper = mountWithTheme(
+      <IconButton
+        iconClassName=""
+        isTooltipContainer={true}
+        onClick={(): void => {}}
+        disabled={true}
+        colour=""
+        iconFontSize="14pt"
+      />,
+      {}
+    )
+    expect(wrapper).toHaveStyleRule('visibility', 'visible', {
+      modifier: `:hover ${StyledTooltip}`,
+    })
+  })
 
-  it('should not present a tooltip if the button is not meant to have a tooltip', (): void => {})
+  it('should not present a tooltip if the button is not meant to have a tooltip', (): void => {
+    const wrapper = mountWithTheme(
+      <IconButton
+        iconClassName=""
+        isTooltipContainer={false}
+        onClick={(): void => {}}
+        disabled={true}
+        colour=""
+        iconFontSize="14pt"
+      />,
+      {}
+    )
+    expect(wrapper).not.toHaveStyleRule('visibility', 'visible', {
+      modifier: `:hover ${StyledTooltip}`,
+    })
+  })
 })
