68f46707dcd92a6ea684c9c1ef348c628b5272a4
Andre Sutherland <andrelsf95@gmail.com>
2019-05-15 06:54:06 +0100

    Changed portal host for local dev in example and added test to tooltip component

--- portal/src/components/Tooltip/Tooltip.test.tsx
+++ portal/src/components/Tooltip/Tooltip.test.tsx
@@ -1,1 +1,13 @@
-describe('Tooltip', (): void => {})
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+
+import Tooltip from './Tooltip'
+import { StyledTooltip } from './tooltip.styles'
+
+describe('Tooltip', (): void => {
+  it('should render without any issues and contain the provided text', (): void => {
+    const rendered = TestRenderer.create(<Tooltip text="Save changes" />)
+    expect(rendered.root.props.text).toBe('Save changes')
+    expect(rendered.root.findAllByType(StyledTooltip).length).toBe(1)
+  })
+})
