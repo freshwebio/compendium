e7e1b6e6ab407125f3bec36964be0018ac25f70a
Andre Sutherland <andrelsf95@gmail.com>
2019-07-19 09:50:21 +0100

    tests for utility module functionality

--- api/pkg/utils/handleping_test.go
+++ api/pkg/utils/handleping_test.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestHandlePingForPingRequest(t *testing.T) {
+	response := HandlePing(events.APIGatewayProxyRequest{
+		Body: "{\"ping\":true}",
+	})
+	if response == nil {
+		t.Errorf("Expected ping response but received nil instead")
+	}
+
+	if response.StatusCode != 200 {
+		t.Errorf("Expected 200 response status code for a ping request but received %d", response.StatusCode)
+	}
+}
+
+func TestHandlePingForNonPingRequest(t *testing.T) {
+	response := HandlePing(events.APIGatewayProxyRequest{
+		Body: "{\"other_request\":true}",
+	})
+	if response != nil {
+		t.Errorf("Expected nil response for a request that is not a ping but received a response object %+v", response)
+	}
+}
--- api/pkg/utils/headers_test.go
+++ api/pkg/utils/headers_test.go
@@ -1,1 +1,100 @@
 package utils
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSetHeadersWithResponseWriter(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	SetHeaders(recorder, true)
+
+	if recorder.Header().Get("Access-Control-Allow-Origin") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Origin to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Allow-Methods") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Methods to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Allow-Headers") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Headers to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Max-Age") == "" {
+		t.Errorf("Expected the Access-Control-Max-Age to be set but was not")
+	}
+
+	if recorder.Header().Get("Content-Type") != "" {
+		t.Errorf("Expected Content-Type to not be set when corsOnly is passed as true")
+	}
+
+	recorder = httptest.NewRecorder()
+	SetHeaders(recorder, false)
+
+	if recorder.Header().Get("Access-Control-Allow-Origin") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Origin to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Allow-Methods") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Methods to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Allow-Headers") == "" {
+		t.Errorf("Expected the Access-Control-Allow-Headers to be set but was not")
+	}
+
+	if recorder.Header().Get("Access-Control-Max-Age") == "" {
+		t.Errorf("Expected the Access-Control-Max-Age to be set but was not")
+	}
+
+	if recorder.Header().Get("Content-Type") == "" {
+		t.Errorf("Expected Content-Type to be set when corsOnly is passed as false")
+	}
+}
+
+func TestSetHeadersWithoutResponseWriter(t *testing.T) {
+	headers := SetHeaders(nil, true)
+
+	if headers["Access-Control-Allow-Origin"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Origin to be set but was not")
+	}
+
+	if headers["Access-Control-Allow-Methods"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Methods to be set but was not")
+	}
+
+	if headers["Access-Control-Allow-Headers"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Headers to be set but was not")
+	}
+
+	if headers["Access-Control-Max-Age"] == "" {
+		t.Errorf("Expected the Access-Control-Max-Age to be set but was not")
+	}
+
+	if headers["Content-Type"] != "" {
+		t.Errorf("Expected Content-Type to not be set when corsOnly is passed as true")
+	}
+
+	headers = SetHeaders(nil, false)
+
+	if headers["Access-Control-Allow-Origin"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Origin to be set but was not")
+	}
+
+	if headers["Access-Control-Allow-Methods"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Methods to be set but was not")
+	}
+
+	if headers["Access-Control-Allow-Headers"] == "" {
+		t.Errorf("Expected the Access-Control-Allow-Headers to be set but was not")
+	}
+
+	if headers["Access-Control-Max-Age"] == "" {
+		t.Errorf("Expected the Access-Control-Max-Age to be set but was not")
+	}
+
+	if headers["Content-Type"] == "" {
+		t.Errorf("Expected Content-Type to be set when corsOnly is passed as false")
+	}
+}
