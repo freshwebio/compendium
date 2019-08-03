a17709a687fa531faec9872db67e79e74a08287d
Andre Sutherland <andrelsf95@gmail.com>
2019-08-03 17:41:37 +0100

    Fixes for serverless deployment of the api

--- api/deploy-aws-lambdas.sh
+++ api/deploy-aws-lambdas.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 parameters_json="$(<sam-deploy-params.json)"
-parameter_overrides=""
 # Allow only new line separators as the internal field separator for new lines.
 IFS=$'\n'
-for param in $(echo $parameters_json | jq -r "to_entries|map(\"ParameterKey=\(.key),ParameterValue=\(.value|tostring)\")|.[]" ); do
-  if [ -n "$parameter_overrides" ]; then
-    parameter_overrides="$parameter_overrides $param"
-  else
-    parameter_overrides="$param"
-  fi
-done
 
-if "sam deploy --stack-name ${STACK_NAME} --template-file ${PACKAGED_TEMPLATE} --parameter-overrides \"${parameter_overrides}\" --capabilities CAPABILITY_IAM"; then
-  printf "\e[32mSuccessfully deployed SAM application, see above for details\n \e[39m"
-else
-  printf "\e[31mFailed to deploy SAM application, see above for details\n \e[39m"
-fi
+echo "Deploying the api"
+
+sam deploy --stack-name ${STACK_NAME} --template-file ${PACKAGED_TEMPLATE} \
+  --parameter-overrides \
+  $(echo $parameters_json | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]') \
+  --capabilities CAPABILITY_IAM
